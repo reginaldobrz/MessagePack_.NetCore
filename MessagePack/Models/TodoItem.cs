453f8bb65ef27396a24a2c1d64cbaea239b33c7e
Reginaldo Aguiar Morais <reginaldo.morais@conveste.com.br>
2020-06-03 18:35:59 -0300

    Add project files.

---
+++ MessagePack/Models/TodoItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessagePack.Models
+{
+    [MessagePackObject]
+    public class TodoItem
+    {
+        [Key(0)]
+        public long Id { get; set; }
+        [Key(1)]
+        public string Name { get; set; }
+        [Key(2)]
+        public bool IsComplete { get; set; }
+    }
+}
